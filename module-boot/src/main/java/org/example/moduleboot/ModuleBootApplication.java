e9e932b5ec62cff14b292bfbf1b75d84fd706fab
g4dalcom <g4dalcom@gmail.com>
2024-02-25 21:31:58 +0900

    add boot module

---
+++ module-boot/src/main/java/org/example/moduleboot/ModuleBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.moduleboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleBootApplication.class, args);
+    }
+
+}
--- module-boot/src/test/java/org/example/moduleboot/ModuleBootApplicationTests.java
+++ module-boot/src/test/java/org/example/moduleboot/ModuleBootApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.multiproject;
+package org.example.moduleboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MultiProjectApplicationTests {
+class ModuleBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
